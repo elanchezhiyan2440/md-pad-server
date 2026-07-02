6af06f82a906fb8cedb879a08f254466f5cff270
Elanchezhiyan E <elanchezhiyan2440@gmail.com>
2026-07-02 15:11:52 +0530

    Jul 2, 15:11

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
 const postsRouter = require("./routes/postsRouter");
+const cors = require('cors');
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
+
+app.use(cors());
 
 app.use("/users",userRouter);
 app.use("/posts", postsRouter);
--- db/queries.js
+++ db/queries.js
@@ -2,8 +2,15 @@ const pool = require("./pool");
 
 // POSTS
 async function getAllPosts() {
-    const {rows} = await pool.query("SELECT * FROM posts");
-    if(rows.length == 0){
+    const { rows } = await pool.query(`SELECT 
+        p.title AS title, 
+        p.id AS pid, 
+        p.content AS content, 
+        u.fullname AS author_name 
+        FROM 
+        posts p JOIN users u 
+        ON u.id = p.author_id;`);
+    if (rows.length == 0) {
         return null;
     };
     return rows;
